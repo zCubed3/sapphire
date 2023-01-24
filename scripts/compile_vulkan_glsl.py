d6b3b96a052e816097a0112dadbcf053492efc85
zCubed3 <liamr2023@gmail.com>
2023-01-23 18:52:06 -0800

    No meaningful changes

--- scripts/compile_vulkan_glsl.py
+++ scripts/compile_vulkan_glsl.py
@@ -7,13 +7,14 @@ import sys
 import subprocess
 
 out_path = sys.argv[1]
+temp_out_path = out_path + ".merge_temp.glsl"
 
 source = ""
 for i in range(2, len(sys.argv)):
     with open(sys.argv[i], 'r') as src_file:
         source += src_file.read()
 
-with open(out_path + ".merge_temp", "w") as merge_file:
+with open(temp_out_path, "w") as merge_file:
     merge_file.write(source)
 
 subprocess.run(["glslc"])
