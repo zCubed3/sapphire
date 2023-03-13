b0d90045a528add5700ea1096f00f2157ea9363f
zCubed3 <liamr2023@gmail.com>
2023-03-13 14:22:56 -0700

    What did I change?

--- sapphire/engine/main.cpp
+++ sapphire/engine/main.cpp
@@ -4,6 +4,8 @@
 
 #include <val/val_instance.hpp>
 
+using namespace val;
+
 int main(int argc, char **argv) {
     SDL_Window *sdl_window = SDL_CreateWindow(
             "Sapphire",
@@ -14,7 +16,46 @@ int main(int argc, char **argv) {
             SDL_WINDOW_VULKAN
     );
 
+    ValInstanceCreateInfo val_create_info{};
 
+    val_create_info.engine_name = "Sapphire Engine";
+    val_create_info.application_name = "Sapphire Application";
+
+    // TODO: Optional SDL?
+    // TODO: Allow switching between sRGB and Linear at runtime
+    ValInstancePresentPreferences present_preferences{};
+    present_preferences.use_vsync = true;
+
+    val_create_info.p_present_preferences = &present_preferences;
+
+    val_create_info.p_sdl_window = sdl_window;
+    val_create_info.instance_extensions = ValExtension::get_sdl_instance_extensions(sdl_window);
+    val_create_info.device_extensions = {{VK_KHR_SWAPCHAIN_EXTENSION_NAME, 0}};
+
+    // TODO: Make this a commandline argument instead
+#ifdef DEBUG
+    val_create_info.validation_layers = {
+            {"VK_LAYER_KHRONOS_validation", 0}};
+#endif
+
+    val_create_info.vk_enabled_features.fillModeNonSolid = true;
+
+    val_create_info.requested_queues = {
+            ValQueue::QueueType::QUEUE_TYPE_GRAPHICS,
+            ValQueue::QueueType::QUEUE_TYPE_PRESENT,
+            ValQueue::QueueType::QUEUE_TYPE_TRANSFER,
+    };
+
+    ValInstance* instance = ValInstance::create_val_instance(&val_create_info);
+
+    SDL_Event event;
+    while (true) {
+        while (SDL_PollEvent(&event)) {
+
+        }
+
+
+    }
 
     return 0;
 }
